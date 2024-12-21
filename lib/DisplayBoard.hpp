b80f6463adcd76ddbba9559bbfff4109d27e956d
Michael Balakirsky <65661353+Probability2@users.noreply.github.com>
2024-12-21 14:03:26 +0200

    Add images of pieces in console

--- lib/DisplayBoard.hpp
+++ lib/DisplayBoard.hpp
@@ -8,30 +8,56 @@ const std::vector<char> kEmptyRow(ChessData::kMaxInd, 'o');
 
 const std::vector<char> kBasicNames = {'P', 'N', 'B', 'R', 'Q', 'K'};
 
-const std::vector<std::string> kBishop = {"  |  ",
-                                          " / \ ",
-                                          "|   |",
-                                          "\   /",
-                                        "__|___|_"};
+const std::size_t kSquareLength = 6;
 
-const std::vector<std::string> kPawn =   {"  |‾|  ",
-                                          "|‾   ‾| ",
-                                          "‾‾| |‾‾",
-                                          " /   \ ",
-                                          "|_____| "};
+const std::size_t kSquareHeight = 11;
 
-const std::vector<std::string> kRook =   {"|‾|_|‾|_|‾|",
-                                          "|         |",
-                                          "‾‾|		 |‾‾",
-                                          " _|     |_",
-                                          "|_________|"};
+const std::vector<std::string> kEmptySquareImage = {"           ",
+                                                    "           ",
+                                                    "           ",
+                                                    "           ",
+                                                    "           ",
+                                                    "           "};
 
-const std::vector<std::string> kKing =   {"    †   ",
-                                          " __/‾\__ ",
-                                          "\|\_/_|_/ "
-                                          " \|||||/",
-                                          "  |||||  ",
-                                          " /_____\ "};
+const std::vector<std::string> kBishopImage = {"     |     ",
+                                               "    / \\    ",
+                                               "   |   |   ",
+                                               "   \\   /   ",
+                                               "  __|___|_ "};
+
+const std::vector<std::string> kPawnImage =   {"   _|=|_   ",
+                                               "  |__ __|  ",
+                                               "    | |    ",
+                                               "   /   \\   ",
+                                               "  |_____|  "};
+                                               
+const std::vector<std::string> kRookImage =   {"|-|_|-|_|-|",
+                                               "|__     __|",
+                                               "  |     |  ",
+                                               " _|     |_ ",
+                                               "|_________|"};
+
+const std::vector<std::string> kKingImage =   {"      +_   ",
+                                               "   __/ \\__ ",
+                                               "  \\|\\_/_|_/",
+                                               "   \\|||||/ ",
+                                               "    |||||  ",
+                                               "   /_____\\ "};
+
+const std::vector<std::string> kQuennImage =  {"   \\\\ | // ",
+                                               "    \\_|_/  ",
+                                               "    |||||  ",
+                                               "    -----  ",
+                                               "   /|||||\\ "};
+
+const std::vector<std::string> kKnightImage = {"   /|___   ",
+                                               "  /   _ \\  ",
+                                               "  \\_/\\ / \\ ",
+                                               "     /   |_",
+                                               "    /_____|"};
+
+const std::vector<std::vector<std::string>> kPieceConsoleImages = {kPawnImage, kKnightImage, kBishopImage,
+                                                             kRookImage, kQuennImage, kKingImage};
 
 class Display {
 public:
@@ -53,16 +79,20 @@ public:
 
   void Set() override;
 
-  std::vector<std::vector<char>> GetBoard() const;
+  std::vector<std::vector<std::vector<std::string>>> GetBoard() const;
 
   void Print();
 
 private:
-  std::vector<std::vector<char>> cboard_;
+  std::vector<std::vector<std::vector<std::string>>> cboard_;
 
-  void SetPossPiece(const std::vector<std::string>& poss, const char name);
+  void PrintSquare(const std::vector<std::string>& vec, const std::size_t ind) const;
 
-  void SetPossPiece(const std::string& pos, const char name);
+  void SetPossPiece(const std::vector<std::string>& poss, const std::vector<std::string>& name);
+
+  void SetPossPiece(const std::string& pos, const std::vector<std::string>& name);
+
+  inline void PrintLine() const;
 
 };
 
