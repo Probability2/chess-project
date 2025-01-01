700744621a13c8455d978d39e4d1fd8cdf646d48
Michael Balakirsky <65661353+Probability2@users.noreply.github.com>
2025-01-01 02:01:16 +0200

    Optimizing main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@
 #include "lib/DisplayBoard.hpp"
 #include "lib/FileManager.hpp"
 #include "parser/ArgParser.hpp"
+#include "src/utils/functions.hpp"
 
 #include <conio.h>
 
-const int kEscapeCode = 0x1b;
+constexpr int kEscapeCode = 0x1b;
 
 int main(int argc, char** argv) {
   system("chcp 65001");
@@ -27,42 +28,22 @@ int main(int argc, char** argv) {
   }
   Board board;
   ConsoleDefault console(board);
+  std::map<std::string, std::function<void(Board&)>> funcs = {{"default", SetDefault}, {"clear", Clear}, {"set", SetPosition}
+                                                             ,{"get from image", GetFromFile}, {"get from file", GetFromFile}
+                                                             ,{"start", Start}, {"save image", SaveFile}, {"save file", SaveFile}
+                                                             ,{"save fen", SaveFEN}, {"escape", EXIT}, {"exit", EXIT}};
   for (;;) {
     if (_getch() == kEscapeCode) {
       return EXIT_SUCCESS;
     }
     std::string request;
     std::getline(std::cin, request);
-    if (request == "default") {
-      board.SetDefault();
-    } else if (request == "clear") {
-      board.Clear();
-    } else if (request == "set") {
-      board.SetPosition();
-    } else if (request == "get from image" || request == "get from file") {
-      std::cout << "File name: ";
-      std::getline(std::cin, request);
-      FileManager::SetValue(board);
-      FileManager::GetFromImage(request);
-
-      //board.GetFromImage(request);
-    } else if (request == "escape") {
-      return EXIT_SUCCESS;
-    } else if (request == "change lang") {
-
-    } else if (request == "start") {
-      Game game;
-    } else if (request == "save image" || request == "save file") {
-      FileManager::SetValue(board);
-      FileManager::SaveImage();
-    } else if (request == "save fen") {
-      FileManager::SetValue(board);
-      FileManager::SaveFEN();
-    } else {
-      continue;
+    auto it = funcs.find(request);
+    if (it != funcs.end()) {
+      funcs[request](board);
     }
-    //system("cls");
 
+    system("cls");
     console.Print();
   }
 
